8ff845766a7fdcbfb90b3129ad680f229fd93c42
AkiTuomaala <64587318+AkiTuomaala@users.noreply.github.com>
2023-04-21 00:11:57 +0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import GanttChart from './components/Gantt';
+
+function App() {
+  return (
+    <div>
+      <GanttChart />
+    </div>
+  );
+}
+
+export default App;
---
+++ README.md
@@ -0,0 +1,3 @@
+# reactTs-gantt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-uomkjm)
